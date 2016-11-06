6a1b8b204bf0fc927c85faea44290a61a2c0b458
allyusd <allyusd@yahoo.com.tw>
2016-11-06 08:35:19 +0800

    change _Build.bat for general project name

--- cmake/_Build.bat
+++ cmake/_Build.bat
@@ -1,6 +1,7 @@
 SET CMAKE_PREFIX_PATH=C:/Qt/5.7/msvc2015
 SET PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin
+SET BuildPath=_build
 
 sh run.sh
 
-msbuild _build/Qt-Websocket.sln
+for %%i in (%BuildPath%/*.sln) do msbuild %BuildPath%/%%~i
